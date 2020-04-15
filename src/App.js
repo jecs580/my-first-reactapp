cdd39640898bb4867bfe56b4f0fd8a22e45ec0ea
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-04-15 19:12:06 -0400

    Uso de los estados en los componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function Helloword(props){
-  // Los componentes por lo se coloca la primera letra en mayuscula
+// function Helloword(props){
+//   // Los componentes por lo se coloca la primera letra en mayuscula
   
-  return (
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-      </div>
-  )
+//   return (
+//     <div id="hello">
+//       <h3>{props.subtitle}</h3>
+//       {props.mytext}
+//       </div>
+//   )
+// }
+
+class Helloword extends React.Component{
+  state = {
+    // Estados de un componente
+    show:true
+  }
+  toggleShow = () =>{
+    this.setState({show:!this.state.show})
+  }
+  render(){
+    if (this.state.show){
+      return(
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        <button onClick={this.toggleShow}>Cambiar de estado</button>
+        </div>
+      )
+    }
+    else{
+      return <h1>No hay elementos
+      <button onClick={this.toggleShow}>Cambiar de estado</button>
+      </h1>
+    }
+  }
 }
+
 // En forma de funcion de flecha
 // const App = ()=> <div>Este es mi componente por medio de una funcion de flecha: <Helloword/></div>
 
