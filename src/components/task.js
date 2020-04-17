eeccd7c6232e274e3bd54b9e62692571d829fd7f
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-04-17 19:01:16 -0400

    Uso de proptypes en los componentes

--- src/components/task.js
+++ src/components/task.js
@@ -1,23 +1,25 @@
 import React,  {Component} from 'react';
-import './task.css'
+import './task.css';
+import PropTypes from 'prop-types';
+
 class Task extends Component{
     styleCompleted(){
         return{
             fontSize:'20px',
-            color: this.props.e.done ? 'gray':'black',  // condicional de una sola linea
-            textDecoration: this.props.e.done ? 'line-through':'none'
+            color: this.props.task.done ? 'gray':'black',  // condicional de una sola linea
+            textDecoration: this.props.task.done ? 'line-through':'none'
         } // Retornamos un objeto con estilos
     }
     render(){
         // Usamos el div por que los componentes solo pueden retornar un elemento a la vez. Cone div encapsulamos los 3 en 1.
 
-        const {e}= this.props;  // Podemos guardar el objeto que envia antes de retornalo, para que se entienda mejor el codigo
-        const redColor={background:'red'}  // Forma de colocar estilos a una etiqueta directamente.
+        const {task}= this.props;  // Podemos guardar el objeto que envia antes de retornalo, para que se entienda mejor el codigo
+        //const redColor={background:'red'}  // Forma de colocar estilos a una etiqueta directamente.
         return ( <div style={this.styleCompleted()}>
-            {this.props.e.title} -
-            {e.description} -
-            {this.props.e.done} -
-            {e.id}
+            {this.props.task.title} -
+            {task.description} -
+            {this.props.task.done} -
+            {task.id}
                 <input type="checkbox"/>
                 <button style={btnDelete}>
                     X
@@ -27,7 +29,9 @@ class Task extends Component{
          
     }
 }
-
+Task.propTypes = {
+    task: PropTypes.object.isRequired  // Colocamos como clave, el nombre que colocamos como props en el componente superior.
+}
 // Agregar estilos atravez de objetos de javascript
 const btnDelete={
     fontSize:'18px',
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import Task from './task'
+import PropTypes from 'prop-types'
+
 class Tasks extends Component {
     render(){
 
         // Recorremos el array por medio de el operador map. de rxjs
-        return this.props.tareas.map(e => <Task key={e.id} e={e}/> ) 
+        return this.props.tareas.map(task => <Task task={task} key={task.id}/> ) 
         // Colocamos el key por que cada elemento hijo que creamos deber tener un key unico.
     }
+}
+
+Tasks.propType={
+    tareas:PropTypes.array.isRequired
 }
 export default Tasks
